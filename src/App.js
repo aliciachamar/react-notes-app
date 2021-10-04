01537f6b057dd8815d125e2e2f31a29a6b8dd04a
aliciachamar <aliciachamar@gmail.com>
2021-10-04 10:18:28 -0600

    added header and body components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>My App</h1>
+      <Header />
+      <Body />
     </div>
   );
 }
